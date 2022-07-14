8506f45f70e85b90d931333e8b6809e9bf662bfa
wendelle <superwendell46@gmail.com>
2022-07-14 18:30:16 -0400

    add classes with an example

---
+++ oop/constructors/es6_classes.js
@@ -0,0 +1,15 @@
+// ES6 Class
+
+class Elf {
+  constructor(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+  attack() {
+    return "attack with " + this.weapon;
+  }
+}
+
+const peter = new Elf("Peter", "stones");
+console.log(peter.attack());
+console.log(peter instanceof Elf);
