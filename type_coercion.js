ca6e5d4d1ec251866930f0dab8ac87bc16f5146d
wendelle <superwendell46@gmail.com>
2023-05-24 22:09:07 -0400

    move files to right folders

--- type_coercion.js
+++ type_coercion.js
@@ -13,3 +13,16 @@ console.log(`"" == {}:`, "" == {});
 console.log(`0 == []:`, 0 == []);
 console.log(`0 == {}:`, 0 == {});
 console.log(`0 == null:`, 0 == null);
+
+// Implicit conversion - Coercion
+// JS auto converts one data type to another
+console.log(1 + 2); // 3
+
+console.log("1" + 2); // 12 -> js converted 2 into "2" then concatenated both strings
+console.log("1" * 2); // 2 -> converted 1 to an int because mult/div can only be done with ints
+
+// Explicit conversion - Type Casting
+// Programmer manually converts one data type to another
+
+console.log(Number("2")); // 2 -> returns a number 2
+console.log(String(2)); // 2 -> returns a string 2
